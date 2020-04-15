9b081d58b21ced2c6dc85bdd2be2033ccea8dfcf
Jhon Alexis Piracoca Arcos <62612262+AlexisPiracoca@users.noreply.github.com>
2020-04-15 10:35:20 -0500

    Update Main.java

--- EJERCICIOS DE JAVA/P2_T5_MULTIPLES/src/usta/sistemas/Main.java
+++ EJERCICIOS DE JAVA/P2_T5_MULTIPLES/src/usta/sistemas/Main.java
@@ -13,7 +13,7 @@ public class Main {
     public static void f_menu() {
         //Description: this method show the menu of this software
         System.out.println("  ==============================  ");
-        System.out.println("||---------SoftMultiplos------||");
+        System.out.println("||---------SoftMultiplos--------||");
         System.out.println("||----Version 1.0 2020-04-14----||");
         System.out.println("||---Created by: Jhon Alexis----||");
         System.out.println("  ==============================  ");
